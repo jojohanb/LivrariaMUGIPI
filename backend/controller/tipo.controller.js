06823cbd3796b358908e5ee159e540b0be16a02b
jojohanb <randesungasabonete@gmail.com>
2025-07-03 12:35:20 -0300

    AAAAAAAAAAAAA

--- backend/controller/tipo.controller.js
+++ backend/controller/tipo.controller.js
@@ -2,63 +2,63 @@ const express = require('express');
 const router = express.Router();
 const tipoDAO = require('../model/tipo.dao');
 
-// ROTA PARA CADASTRAR UMA EDITORA
+// ROTA PARA CADASTRAR UM TIPO
 router.post('/cadastrar', async (req, res) => {
   try {
-    if (!('nome_editora' in req.body) || req.body.nome_editora.trim() === '') {
-      return res.status(400).json({ error: 'Nome da editora é obrigatório.' });
+    if (!('descricao' in req.body) || req.body.descricao.trim() === '') {
+      return res.status(400).json({ error: 'Crie um tipo válido.' });
     }
-    const editoraCriada = await editoraDAO.cadastrarEditora(req.body.nome_editora.trim());
-    res.status(201).json({ message: 'Editora criada com sucesso!', editora: editoraCriada }); // MELHORIA
+    const tipoCriado = await editoraDAO.cadastrarTipo(req.body.descricao.trim());
+    res.status(201).json({ message: 'Tipo criado com sucesso!', tipo: tipoCriado }); // MELHORIA
   } catch (erro) {
-    res.status(500).json({ erro: 'Erro na função de cadastro de editora do controller.' });
+    res.status(500).json({ erro: 'Erro na função de cadastro de tipo do controller.' });
   }
 });
 
 // ROTA DE LISTAGEM DE TODAS AS EDITORAS
 router.get('/listar', async (req, res) => {
   try {
-    const editora = await editoraDAO.listarEditora();
-    res.status(200).json({ editora });
+    const tipo = await tipoDAO.listarTipo();
+    res.status(200).json({ tipo });
   } catch (erro) {
-    res.status(400).json({ erro: 'Erro ao listar editora' });
+    res.status(400).json({ erro: 'Erro ao listar tipos' });
   }
 });
 
 // ROTA DE ATUALIZAR AS EDITORAS
 router.put('/:id', async (req, res) => {
-  const id_editora = parseInt(req.params.id);
-  const { nome_editora } = req.body;
+  const tipo_id = parseInt(req.params.id);
+  const { descricao } = req.body;
 
-  if (!nome_editora || isNaN(id_editora)) {
-    return res.status(400).json({ mensagem: 'Nome e ID válidos são obrigatórios.' });
+  if (!descricao || isNaN(tipo_id)) {
+    return res.status(400).json({ mensagem: 'Id e desc válidos são obrigatórios.' });
   }
 
   try {
-    const editoraAtualizado = await editoraDAO.atualizarEditora(nome_editora.trim(), id_editora);
-    if (!editoraAtualizado) {
-      return res.status(404).json({ mensagem: 'Editora não encontrada.' });
+    const tipoAtualizado = await tipoDAO.atualizarTipo(descricao.trim(), tipo_id);
+    if (!tipoAtualizado) {
+      return res.status(404).json({ mensagem: 'Tipo não encontrado.' });
     }
-    res.status(200).json(editoraAtualizado);
+    res.status(200).json(tipoAtualizado);
   } catch (erro) {
-    res.status(500).json({ mensagem: 'Erro ao atualizar editora.', erro: erro.message });
+    res.status(500).json({ mensagem: 'Erro ao atualizar tipo.', erro: erro.message });
   }
 });
 
 // ROTA DE DELETAR AS EDITORAS
 router.delete('/deletar', async (req, res) => {
   try {
-    const { nome_editora } = req.body;
-    if (!nome_editora || nome_editora.trim() === '') {
-      return res.status(400).json({ erro: 'Nome da editora é obrigatório para deletar.' });
+    const { tipo_id } = req.body;
+    if (!tipo_id || tipo_id.trim() === '') {
+      return res.status(400).json({ erro: 'id é obrigatório para deletar.' });
     }
-    const editoraDeletada = await editoraDAO.deletarEditora(nome_editora.trim());
-    if (!editoraDeletada) {
-      return res.status(404).json({ mensagem: 'Editora não encontrada.' });
+    const tipoDeletado = await tipoDAO.deletarTipo(descricao.trim());
+    if (!tipoDeletado) {
+      return res.status(404).json({ mensagem: 'Tipo não encontrado.' });
     }
-    res.status(200).json({ mensagem: 'Editora deletada com sucesso.', editora: editoraDeletada });
+    res.status(200).json({ mensagem: 'Tipo deletado com sucesso.', tipo: tipoDeletado });
   } catch (erro) {
-    res.status(500).json({ erro: 'Erro na função de deletar editora do controller.' });
+    res.status(500).json({ erro: 'Erro na função de deletar tipo do controller.' });
   }
 });
 
